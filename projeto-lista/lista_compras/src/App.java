0cb8df96a64dbf04c3e8c99f30768b536f8aa4b4
TakeOno <ftohno@gmail.com>
2026-06-12 20:46:47 -0300

    salvando array

--- projeto-lista/lista_compras/src/App.java
+++ projeto-lista/lista_compras/src/App.java
@@ -1,5 +1,28 @@
+import java.util.Arraylist;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner inUser =new Scanner(System.in)
+        ArrayList<String> listacompras= new Arraylist<>();
+        int opcaolista = 0;
+        
+        do{
+        System.out.println("""
+                escolha a opçãodesejada
+                1 -Criar uma nova lista de compras
+                2- Sair
+                """);
+                int opcaolista = inUser.nextInt();
+                inUser.nextLine();
+
+                switch (opcaolista) {
+                    case 1 -> System.out.println("Lista criada com sucesso");
+                    case 2 -> System.out.println("Obrigado por usar o nosso sistema");
+                                        
+                    
+                    default -> System.out.println("opcao Invalida");
+                    
+                }
+        }while(opcaolista !=2);    
     }
 }
